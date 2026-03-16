e4489a618951ca1f2d80a0e3c78cb9f32e51a295
Yann Brochet <yann.brochet@etu.univ-tours.fr>
2026-03-16 11:13:52 +0100

    YB - Commit 2 : Mise en place des callbacks pour la page 1.

--- app.py
+++ app.py
@@ -17,5 +17,10 @@ app.layout = dbc.Container([
     dash.page_container 
 ], fluid=True)
 
+import pages.table_cb 
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
